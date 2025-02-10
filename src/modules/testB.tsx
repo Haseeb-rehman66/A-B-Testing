4e8470680e00a9ee7a43515bf0d7ea146480b8a1
haseeb-rehman66 <haseeb_rehman666@yahoo.com>
2025-02-10 16:19:23 +0500

    testB file edit

---
+++ src/modules/testB.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestB: React.FC = () => {
+  return (
+    <div>
+      <h1>Test B Component</h1>
+      <p>This is a basic component for A/B testing.</p>
+    </div>
+  );
+};
+
+export default TestB;
